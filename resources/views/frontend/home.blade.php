3f1db67f4bff9b5824b89c6e0a126c51fa700678
Htet Myat Soe <74512999+hms-19@users.noreply.github.com>
2023-12-14 14:34:02 +0630

    update

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -335,7 +335,7 @@
     <div class="modal-dialog modal-dialog-centered modal-lg">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="notificationLabel">Latest Messages</h5>
+          <h5 class="modal-title" id="notificationLabel">Latest Announcements</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
